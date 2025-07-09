2e449b9f5ee1e4bb7ee29f5e3f89f0bfd25153b5
Umang Singh <umang8933@gmail.com>
2025-07-10 00:23:07 +0530

    Compliting 2 challenge

--- src/StudentChallenge.java
+++ src/StudentChallenge.java
@@ -2,18 +2,21 @@ import java.io.*;
 public class StudentChallenge {
     public static void main(String[] args) {
         try{
-            FileInputStream fis = new FileInputStream("D:/Java/Java Language/java/Practice work/InterfacePractice/Test.txt");
-            byte[] b = new byte[fis.available()];
-            fis.read(b);
+            FileInputStream fis1 = new FileInputStream("D:/Java/Java Language/java/Practice work/InterfacePractice/Test.txt");
+            byte[] b = new byte[fis1.available()];
+            fis1.read(b);
             String str = new String(b);
             String store = str;
             System.out.println(store);
-            FileOutputStream fos = new FileOutputStream("D:/Java/Java Language/java/Practice work/InterfacePractice/Challenge.txt");
+            FileOutputStream fos1 = new FileOutputStream("D:/Java/Java Language/java/Practice work/InterfacePractice/Challenge1.txt");
             byte[] d = store.getBytes();
-            fos.write(b,0,str.length());
-            fos.close();
-            fis.close();
-
+            fos1.write(b,0,str.length());
+            fos1.close();
+            fis1.close();
+            //2nd challenge:-
+            FileInputStream fis2 = new FileInputStream("D:/Java/Java Language/java/Practice work/InterfacePractice/Test.txt");
+            FileInputStream fis3 = new FileInputStream("D:/Java/Java Language/java/Practice work/InterfacePractice/Challenge1.txt");
+            FileOutputStream fos2 = new FileOutputStream("D:/Java/Java Language/java/Practice work/InterfacePractice/Challenge2.txt");
         } catch (Exception e) {
             System.out.println(e);
         }
