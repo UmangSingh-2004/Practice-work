ef1c622997be5ddafd4ff66f15599bb0bf92d536
Umang Singh <umang8933@gmail.com>
2025-08-14 01:19:53 +0530

    Task

---
+++ src/TestOfStream.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class TestOfStream {
+    public static void main(String[] args) {
+        //First task:- Storing a float type array in a file and read to file
+        //        float [] f = {5.3f,6.3f,6.23f,2.3f,56.2f};
+        try{
+            //storing a float array :-
+//            FileOutputStream fos = new FileOutputStream("D:/Java/Java Language/java/Practice work/InterfacePractice/Test2.txt");
+//            DataOutputStream dos = new DataOutputStream(fos);
+//            dos.writeInt(f.length);
+//            for (float list: f){
+//                dos.writeFloat(list);
+//            }
+//            dos.close();
+//            fos.close();
+            //reading numbers for a file :-
+//            FileInputStream fis = new FileInputStream("D:/Java/Java Language/java/Practice work/InterfacePractice/Test2.txt");
+//            DataInputStream dis = new DataInputStream(fis);
+//            int j = dis.readInt();
+//            for (int i = 1; i <= j; i++){
+//                System.out.println(dis.readFloat());
+//            }
+//            fis.close();
+//            dis.close();
+//----------------------------------------------------------------------------------------------------------------------------------------------------------//
+            //Second task :- Storing object and data in a file
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
